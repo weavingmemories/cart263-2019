efe6c4c3626394ccf405d847f67618df93f77c76
Macey McCormick <red-rainbows@hotmail.com>
2019-04-04 15:43:22 -0400

    a8: added pizzicato basics for synth
    testing if generative music will work with aframe 'click'

--- assignments/assignment8/js/script.js
+++ assignments/assignment8/js/script.js
@@ -9,3 +9,28 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 
 ******************/
+
+let frequencies = [
+  220,246.94,277.18,293.66,329.63,369.99,415.30
+];
+
+let synth;
+
+function setup() {
+  synth = new Pizzicato.Sound({
+    source: 'wave',
+    options: {
+      type: 'sine',
+      frequency: 220
+    }
+  });
+}
+
+function playNote() {
+  // Pick a random frequency from the array
+  let frequency = frequencies[Math.floor(Math.random() * frequencies.length)];
+  // Set the synth's frequency
+  synth.frequency = frequency;
+  // If it's note already play, play the synth
+  synth.play();
+}
