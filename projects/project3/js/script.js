bf82fbc86c89f060e7af8c103cb3db7b0bc25879
Macey McCormick <red-rainbows@hotmail.com>
2019-04-21 22:01:50 -0400

    p3: removed some scales, adding tempo changes

--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -12,91 +12,31 @@ author, and this description to match your project!
 
 let NOTE_TEMPO = 500;
 
-let cFlatMajor = [
-  493.88, 466.16, 415.30, 369.99, 329.63, 311.13, 277.18, 246.94
-];
-
-let gFlatMajor = [
-  369.99, 349.23, 311.13, 277.18, 246.94, 233.08,	207.65, 185.00
-];
-
-let dFlatMajor = [
-  277.18, 261.63, 233.08, 207.65, 185.00, 174.61, 155.56, 138.59
-];
-
-let aFlatMajor = [
-  415.30, 392.00, 349.23, 311.13, 277.18, 261.63, 233.08, 207.65
-];
-
 let eFlatMajor = [
- 311.13, 293.66, 261.63, 233.08, 207.65, 196.00, 174.61, 155.56
-];
-
-let bFlatMajor = [
-  466.16, 440.00, 392.00, 349.23, 311.13, 293.66, 261.63, 233.08
-];
-
-let fMajor = [
- 349.23, 329.63, 293.66, 261.63, 233.08, 220.00, 196.00, 174.61
+  311.13, 293.66, 261.63, 233.08, 207.65, 196.00, 174.61, 155.56
 ];
 
 let cMajor = [
   261.63, 246.94, 220.00, 196.00, 174.61, 164.81, 146.83, 130.81
 ];
 
-let gMajor = [
-  392.00, 369.99, 329.63, 293.66, 261.63, 246.94, 220.00, 196.00
-];
-
-let dMajor = [
-  293.66, 277.18, 246.94, 220.00, 196.00, 185.00, 164.81, 146.83
-];
-
 let aMajor = [
   440.00, 415.30, 369.99, 329.63, 293.66, 277.18, 246.94, 220.00
-];
-
-let eMajor = [
-  329.63, 311.13, 277.18, 246.94, 220.00, 207.65, 185.00, 164.81
-];
-
-let bMajor = [
-  493.88, 466.16, 415.30, 369.99, 329.63, 311.13, 261.63, 246.94
 ];
 
 let fSharpMajor = [
   369.99, 349.23, 311.13, 277.18, 246.94, 233.08, 207.65, 185.00
 ];
 
-let cSharpMajor = [
-  277.18, 261.63, 233.08, 207.65, 185.00, 174.61, 155.56, 138.59
+let eFlatMinor = [
+  311.13, 277.18, 246.94, 233.08, 207.65, 185.00, 174.61, 155.56
 ];
 
-let scaleKeys = [
-  cFlatMajor,
-  gFlatMajor,
-  dFlatMajor,
-  aFlatMajor,
-  eFlatMajor,
-  bFlatMajor,
-  fMajor,
-  cMajor,
-  gMajor,
-  dMajor,
-  aMajor,
-  eMajor,
-  bMajor,
-  fSharpMajor,
-  cSharpMajor
+let gMinor = [
+  392.00, 349.23, 311.13, 293.66, 261.63, 233.08, 220.00, 196.00
 ];
 
-//temporary freq
-
-let frequencies = [
-  220,246.94,277.18,293.66,329.63,369.99,415.30
-];
-
-let currentScale = 8;
+let currentScale = cMajor;
 let effectInterval;
 let effectGain = 0;
 let synth;
@@ -116,53 +56,74 @@ function setup() {
 
   var distortion = new Pizzicato.Effects.Distortion({
     gain: effectGain
-});
+  });
 
-synth.addEffect(distortion);
+  synth.addEffect(distortion);
 
-    $("#StartButton").click(function () {
+  $("#StartButton").click(function() {
     $("#SplashScreen").hide();
     $("#spaceJam").show();
-    interval = setInterval(playNote,NOTE_TEMPO);
-});
+    interval = setInterval(playNote, NOTE_TEMPO);
+  });
 
   let sphere = document.getElementById("addGainPlanet");
   sphere.addEventListener('mouseenter', function() {
- effectInterval = setInterval(function () {
-  let gain = effectGain += 0.1;
-  distortion.gain = gain;
-  console.log(effectGain);
-},1000);
-    console.log("Array + 1");
+    effectInterval = setInterval(function() {
+      let gain = effectGain += 0.1;
+      distortion.gain = gain;
+      console.log(effectGain);
+    }, 500);
+  });
 
-});
+  sphere.addEventListener('mouseleave', function() {
+    console.log("Left");
+    clearInterval(effectInterval);
+  });
 
-sphere.addEventListener('mouseleave', function() {
-console.log("Left");
-clearInterval(effectInterval);
-});
+  let sphere2 = document.getElementById("subGainPlanet");
+  sphere2.addEventListener('mouseenter', function() {
+    effectInterval = setInterval(function() {
+      let gain = effectGain -= 0.1;
+      distortion.gain = gain;
+      console.log(effectGain);
+    }, 500);
+  });
 
-let sphere2 = document.getElementById("subGainPlanet");
-sphere2.addEventListener('mouseenter', function() {
- effectInterval = setInterval(function () {
-  let gain = effectGain -= 0.1;
-  distortion.gain = gain;
-  console.log(effectGain);
-},1000);
-console.log(effectGain);
-  console.log("Array - 1");
-});
+  sphere2.addEventListener('mouseleave', function() {
+    console.log("Left");
+    clearInterval(effectInterval);
+  });
 
-sphere2.addEventListener('mouseleave', function() {
-console.log("Left");
-clearInterval(effectInterval);
-});
+  let sphere3 = document.getElementById("gMinorPlanet");
+  sphere3.addEventListener('mouseenter', function() {
+      currentScale = gMinor;
+    console.log(currentScale);
+  });
+
+  let sphere4 = document.getElementById("eFlatMajorPlanet");
+  sphere4.addEventListener('mouseenter', function() {
+      currentScale = eFlatMajor;
+    console.log(currentScale);
+  });
+
+  let sphere5 = document.getElementById("fastTempoPlanet");
+  sphere5.addEventListener('mouseenter', function() {
+    effectInterval = setInterval(function() {
+      NOTE_TEMPO - 100;
+      console.log(NOTE_TEMPO);
+    }, 500);
+  });
+
+  sphere5.addEventListener('mouseleave', function() {
+    console.log("Left");
+    clearInterval(effectInterval);
+  });
 }
 
 function playNote() {
   // Pick a random frequency from the array
 
-  let frequency = frequencies[Math.floor(Math.random() * frequencies.length)];
+  let frequency = currentScale[Math.floor(Math.random() * currentScale.length)];
   // Set the synth's frequency
   synth.frequency = frequency;
   // If it's note already play, play the synth
